a8fc320256506b6838f33fb5843f8324186eed53
jooeui <kje_0727@naver.com>
2021-11-08 10:23:59 +0900

    js basics #9

---
+++ javascript-practices/basics/ex09.js
@@ -0,0 +1,30 @@
+/* 
+자바스크립트 객체 4
+prototype 기반의 상속과 오버라이딩(자바스크립트 객체지향 프로그래밍)
+*/
+
+var MyObject = function(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+MyObject.prototype.school = 'BIPA';
+MyObject.prototype.course = 'DOUZONE';
+MyObject.prototype.info = function() {
+    console.log(this.name + " : " + this.age + " : " + this.school + " : " + this.course);
+}
+
+// MyObject 객체 생성
+var o1 = new MyObject("이브이", 5);
+o1.school = 'DONGSEO';
+o1.info();
+
+var o2 = new MyObject("님피아", 10);
+o2.info();
+
+// 오버라이딩
+o2.info = function() {
+    console.log('비밀!');
+}
+
+o2.info();
